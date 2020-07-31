11ec8534969c4f99d4922b33fe77e0d08e3569cc
Dan <A01208905@itesm.mx>
2020-07-31 13:57:25 -0500

    [DAN] Finish final project

--- ProyectoFinal/class/vertex.js
+++ ProyectoFinal/class/vertex.js
@@ -1,37 +1,35 @@
 /*
-* Class Body to create Planets, Stars, Asteroids, Moons
+* Author: Daniel Elias
+* Year: 2020
+* Class Vertex called by class Graph to create each vertex for the graph
 */
 
 class Vertex{
-    constructor(radius, position, rotation, rotation_speed, translation_speed, color, material, num_moons, hasRing, orbit_radius, parent_group, isStar, isAsteroid, name){
+    constructor(radius, position, rotation, material, mass, parent_group, id, name){
         //Basics
         this.radius = radius;
-        this.rotation_speed = rotation_speed;
-        this.translation_speed = translation_speed;
-        this.orbit_radius = orbit_radius;
-        this.color = color;
         this.material = material;
+        this.id = id
         this.name = name;
+        this.nameRender = null;
+        this.font = null;
+        this.name_text_mesh = null;
+        this.mass = mass;
+       
+        //Edges
+        this.adjacent_vertices = [];
+        this.non_adjacent_vertices = [];
 
         //Groups
-        this.orbit_group = new THREE.Object3D; //for the orbit
-        this.body_group = new THREE.Object3D; //for the planet/star/moon/ring
+        this.vertex_group = new THREE.Object3D; //for the planet/star/moon/ring
         this.parent_group = parent_group;
-        this.orbit_group.add(this.body_group);//orbit controls the body
-       
+
         //Geometry
-        this.isAsteroid = isAsteroid;
-        if(this.isAsteroid){
-            this.geometry =  new THREE.SphereGeometry(this.radius, 3, 3);
-            //low poly sphere for asteroids
-        } else {
-            this.geometry =  new THREE.SphereGeometry(this.radius, 40, 40);
-        }
+        this.geometry =  new THREE.SphereGeometry(this.radius, 40, 40);
         
         //Mesh
         this.mesh = new THREE.Mesh(this.geometry, this.material);
-        this.body_group.add(this.mesh);
-        this.orbit_group.add(this.body_group);
+        this.vertex_group.add(this.mesh);
 
         //Position / Rotation
         this.position = position;
@@ -43,131 +41,60 @@ class Vertex{
         this.mesh.rotation.y = this.rotation.y;
         this.mesh.rotation.z = this.rotation.z;
 
-        //Is the body is a star, let light pass through
-        this.isStar = isStar;
-        this.mesh.castShadow = (this.isStar) ? false : true;
-        this.mesh.receiveShadow = (this.isStar) ? false : true;
+        //Physical Body
+        this.physicalBody = App.addPhysicalBody(this.mesh, {mass: this.mass}, this.radius);
+        this.physicalBody.collisionResponse = 0;
 
-        //If body has ring - addRing
-        this.hasRing = hasRing;
-        if(this.hasRing) this.addRing();
+        //Shadows
+        this.mesh.castShadow = true;
+        this.mesh.receiveShadow = true;
 
-        //If body is a planet - add orbit
-        if(!this.isAsteroid && !this.isStar){
-            //this.orbit = new Orbit(this.orbit_radius, this.parent_group.position, this.rotation);
-            //this.orbit_group.add(this.orbit.mesh);
-            //bodiesList.push(this);
-        }
+        //Name for collisions
+        this.mesh.name = this.id;
 
-        this.moonsList = []; //keep track of the generated moons
-        this.num_moons = num_moons;
-        //If body is a planet - addMoons
-        if(!this.isStar && !this.isAsteroid) this.addMoons();
-
-        Game.scene.add(this.orbit_group) //add to scene and not to parent
-    }
-
-    addRing(){
-        let segments = 96;
-        //inner radius
-        let start = this.radius * 1.200;
-        //outer radius
-        let end = this.radius * 2.3;
-
-        if(this.name == "uranus"){
-            start = this.radius * 1.600;
-            end = this.radius * 1.950;
-        } 
+        //Adds marker icon
+        this.mark = this.addMark();
         
-        //Ring Geometry
-        this.ring_geometry = new THREE.RingBufferGeometry(start, end, segments);
-       
-        /****This part is to change the ring geometry UV mapping fot the texture****/
-        var uvs = this.ring_geometry.attributes.uv.array;
-        var phiSegments = this.ring_geometry.parameters.phiSegments || 0;
-        var thetaSegments = this.ring_geometry.parameters.thetaSegments || 0;
-        phiSegments = phiSegments !== undefined ? Math.max( 1, phiSegments ) : 1;
-        thetaSegments = thetaSegments !== undefined ? Math.max( 3, thetaSegments ) : 8;
-        for ( var c = 0, j = 0; j <= phiSegments; j ++ ) {
-            for ( var i = 0; i <= thetaSegments; i ++ ) {
-                uvs[c++] = i / thetaSegments,
-                uvs[c++] = j / phiSegments;
-            }
-        }
-        /*****************************************************************************/
+        //Add to the interactables group
+        App.collisionGroup.add(this.mesh);
 
-        //materials
-        this.ring_material = materials["saturn_ring"]; 
-        if(this.name == "saturn"){
-            this.ring_geometry.rotateX(1.7);
-        } else if(this.name == "uranus"){
-            this.ring_material = materials["uranus_ring"]; 
-        }
-
-        //Ring Mesh
-        this.ring_mesh = new THREE.Mesh(this.ring_geometry, this.ring_material);
-        this.body_group.add(this.ring_mesh);
+        //Add to parent
+        this.parent_group.add(this.vertex_group) 
     }
 
-    addMoons(){
-        //New group for the moons
-        this.moons = new THREE.Object3D; //moons group for the body
-        this.body_group.add(this.moons)
-    
-        //Create a moon for the specified number of moons
-        for(let i = 1; i <= this.num_moons; i++){
-            //set material
-            let material_moon = Game.materials.moon;
-            //special moons for mars
-            if(this.name == "mars"){
-                if(i-1 == 0) material_moon = Game.materials.phobos;
-                else material_moon = Game.materials.deimos;
-            }
-            //Create a mesh and body (without moons) for the currentmoon
-            let moon = new Body(
-                this.radius/(this.radius), 
-                {
-                    x: this.position.x + (i/3) * this.radius + (this.radius*2.5), //offset from body
-                    y: this.position.y, 
-                    z: this.position.z
-                },
-                {
-                    x: this.rotation.x, 
-                    y: this.rotation.y, 
-                    z: this.rotation.z
-                }, 
-                getRandomFloat(1,5), //random rotation speed
-                getRandomFloat(1,3), //random translation speed
-                0xffffff,
-                material_moon,
-                0,
-                false, 
-                (i/3) * this.radius + (this.radius*2.5), //orbit
-                this.orbit_group, //parent group
-                false,
-                false,
-                "moon"
-            );
-            //Add moons to group
-            this.moons.add(moon.orbit_group);
-            this.moonsList.push(moon);
-            //bodiesList.push(moon); //bodiesList will animate moons
-        }
-        
-        //Position the moons of the planets at different locations within a circunference
-        let angle_fraction = (2*Math.PI) / this.moonsList.length;
-        let angle_origin = Math.PI/2;
-        let x = 0;
-        let z = 0;
-        let i = 0
-        for(let moon_body of this.moonsList){
-            this.moons.add(moon_body.orbit_group);
-            let angle = angle_fraction * i + angle_origin;
-            x = Math.cos(angle) * moon_body.orbit_radius
-            z = Math.sin(angle) * moon_body.orbit_radius 
-            moon_body.mesh.position.x = x;
-            moon_body.mesh.position.z = z;
-            i += 1;
-        }
+    /**
+     * Adds the name of the vertex on top of it, in case the vertex has a name
+     * @param {string} text 
+     * @param {THREE Font} font 
+     * @param {Vertex} vertex 
+     */
+    addNameRender(text, font, vertex){
+        this.font = font
+        //Text will be a TextGeometry
+        var text_geometry = new THREE.TextGeometry( text, {
+            font: font,
+            size: 80,
+            height: 5,
+            curveSegments: 12,
+        } );
+        let color = new THREE.Color();
+        color.setHex(0x5f00c4); //purple
+        let textMaterial = new THREE.MeshBasicMaterial({ color: color });
+        this.name_text_mesh = new THREE.Mesh(text_geometry , textMaterial);
+        this.name_text_mesh.scale.set(0.01, 0.01, 0.01)
+        this.name_text_mesh.position.x = - this.radius / 2
+        this.name_text_mesh.position.y = + this.radius * 5.5 //center text
+        this.name_text_mesh.position.z = 0
+        this.name_text_mesh.name = vertex.mesh.name
+        vertex.mesh.add(this.name_text_mesh); //add to the vertex mesh group
     }
+
+    /**
+     * Adds a mark icon obj model on top of the vertex
+     */
+    addMark(){
+        let objModel = {obj:'../models/obj/location/LocationMarkIcon.obj', map:'../images/emojis/wink.png', map2:'../images/emojis/red.png' };
+        this.mark = App.loadObj(objModel, this.mesh);
+    }
+
 }
--- ProyectoFinal/util/utils.js
+++
@@ -1,7 +0,0 @@
-function getRandomFloat(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
